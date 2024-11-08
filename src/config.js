61d484f4c09b54bab1071b37dc09edfa17d18cd3
xmrjun <3211351132@qq.com>
2024-11-09 01:33:14 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,11 +1,10 @@
 export const config = {
   WEBSOCKET_URL: 'wss://proxy2.wynd.network:4650/',
-  VERSION: '2.0.0',
-  PING_INTERVAL: 20000,
+  VERSION: '4.28.1',
+  PING_INTERVAL: 30000,
   MAX_RETRIES: 5,
-  HANDSHAKE_TIMEOUT: 15000,
-  RECONNECT_DELAY: 5000,
-  MAX_RECONNECT_DELAY: 30000,
+  HANDSHAKE_TIMEOUT: 10000,
+  RECONNECT_DELAYS: [1000, 2000, 5000, 10000],
   SECURITY: {
     VALID_PROXY_REGEX: /^(http|https|socks[45]):\/\/([^:]+:[^@]+@)?([a-zA-Z0-9.-]+):\d{1,5}$/
   }
